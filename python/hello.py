e95eab715156431df1bbdcddc461c1e2f6e077df
choreodev <aaron.jennings+github@temboo.com>
2016-09-07 09:02:02 -0400

    latest commit

---
+++ python/hello.py
@@ -0,0 +1,13 @@
+// Instantiate the Choreo, using a previously instantiated TembooSession object, eg:
+TembooSession session = new TembooSession("ACCOUNT_NAME", "APP_KEY_NAME", "APP_KEY_VALUE");
+
+HelloWorld helloWorldChoreo = new HelloWorld(session);
+
+// Get an InputSet object for the choreo
+HelloWorldInputSet helloWorldInputs = helloWorldChoreo.newInputSet();
+
+// Set inputs
+helloWorldInputs.set_Value("721955928");
+
+// Execute Choreo
+HelloWorldResultSet helloWorldResults = helloWorldChoreo.execute(helloWorldInputs);
